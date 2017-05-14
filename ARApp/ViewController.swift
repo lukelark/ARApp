af233a5da7375e81c0a637170ca8578d5767523e
vkrusen <victorkrusenstrahle@gmail.com>
2017-05-14 15:55:55 +0200

    0.0.1

--- ARApp/ViewController.swift
+++ ARApp/ViewController.swift
@@ -16,7 +16,24 @@ class ViewController: ARCameraViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func setupContent() {
+    override func setupContent() {
+        
+        // Initialise image trackable
+        let imageTrackable = ARImageTrackable(image: UIImage(named: "spaceMarker.jpg"), name: "space")
+        
+        // Get instance of image tracker manager
+        let trackerManager = ARImageTrackerManager.getInstance()
+        trackerManager?.initialise()
+        
+        // Add image trackable to image tracker manager
+        trackerManager?.addTrackable(imageTrackable)
+        
+        // Initialise image node
+        let imageNode = ARImageNode(bundledFile: "eyebrow.png")
+        
+        // Add image node to image trackable
+        imageTrackable?.world.addChild(imageNode)
+
     }
 
     override func didReceiveMemoryWarning() {
